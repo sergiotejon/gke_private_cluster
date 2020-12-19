107eae60863335103a6a2b336a95ba88c6c48b32
Sergio Tejón <sergio.tejon@gmail.com>
2020-12-19 22:35:23 +0100

    script terraform output

--- run_bastion.sh
+++ run_bastion.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 NET_IFACE=eth0
-GKE_CLUSTER_LOCAL_IP=172.16.0.2
+GKE_CLUSTER_NAME=$(terraform output -raw kubernetes_cluster_name)
+GKE_CLUSTER_LOCAL_IP=$(terraform output -raw kubernetes_cluster_private_endpoint)
 GKE_CLUSTER_PORT=443
 LOCAL_PORT=8443
-BASTION_PUBLIC_IP=35.195.143.155
+BASTION_PUBLIC_IP=$(terraform output -raw bastion_public_ip)
 BASTION_USER=stejon
+
 
 if [ ! -f ./terraform.tfvars ] ; then
    echo "Este script ha de ser ejecutado en el directorio donde se ubica la arquitectura en código con Terraform."; exit 1
 fi
 
-kubectl config set clusters.$(kubectl config get-contexts | grep $(cat ./terraform.tfvars | grep project_id | cut -d'=' -f2 | tr -d "\" ") | awk '{print $2}').server https://$GKE_CLUSTER_LOCAL_IP:$LOCAL_PORT
+kubectl config set clusters.$(kubectl config get-contexts | grep $GKE_CLUSTER_NAME | awk '{print $2}').server https://$GKE_CLUSTER_LOCAL_IP:$LOCAL_PORT
 
 sudo ip addr add $GKE_CLUSTER_LOCAL_IP/32 dev $NET_IFACE 2> /dev/null
 
